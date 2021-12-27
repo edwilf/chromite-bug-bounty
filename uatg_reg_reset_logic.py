d0393ff709c356caf4bcb0377274421d006d29f4
prajwal-billawa <46377316+prajwal-billawa@users.noreply.github.com>
2021-12-27 21:09:33 +0530

    Update uatg_reg_reset_logic.py

--- uatg_reg_reset_logic.py
+++ uatg_reg_reset_logic.py
@@ -1,6 +1,5 @@
 from yapsy.IPlugin import IPlugin
-from uatg.instruction_constants import base_reg_file, arithmetic_instructions,\
-    bit_walker
+from uatg.instruction_constants import base_reg_file, arithmetic_instructions,bit_walker
 from typing import Dict, List, Union, Any
 import random
 
@@ -15,34 +14,34 @@ class uatg_reg_reset_logic(IPlugin):
         super().__init__()
         self.isa = 'RV32I'
         self.isa_bit = 'rv32'
-        self.offset_inc = 4
         self.xlen = 32
-        self.num_rand_var = 100
 
     def execute(self, core_yaml, isa_yaml) -> bool:
         self.isa = isa_yaml['hart0']['ISA']
         if 'RV32' in self.isa:
             self.isa_bit = 'rv32'
             self.xlen = 32
-            self.offset_inc = 4
         else:
             self.isa_bit = 'rv64'
             self.xlen = 64
-            self.offset_inc = 8
         return True
 
     def generate_asm(self) -> List[Dict[str,Union[Union[str, List[Any]], Any]]]:
+	""" 
+	Checks the reset values of registers
+	Method similar to OR Reduction
+	Iteratively OR contents of the registers
+	in register file and test if 0 at the end
+	"""
         reg_file = base_reg_file.copy()
         test_dict = []
-	""" We would check the register x0 and x1 for equality.
-	The for loop is used to perform the or operation of register x1 with all the remaining 		registers.
-	"""
-        asm_code = "\n\tbne x1,x0,LOC"
+	
+        asm_code = "\n\tbne x1,x0,LOC"		# Check if x1 is 0 and if yes, use a sum register
         for rs in reg_file:
-            asm_code += f'\n\tor x1,x1,{rs}'
+            asm_code += f'\n\tor x1,x1,{rs}'	# Perform OR Reduction
         asm_code += "\n\nLOC:\n"
-        asm_code += "\tslt x1,x1,x0"
-        compile_macros = []
+        asm_code += "\txor x1,x1,x0"		# Test if final value is 0
+        compile_macros = []			# If all registers reset, final value of x1 is 0
 
         test_dict.append({
             'asm_code': asm,
