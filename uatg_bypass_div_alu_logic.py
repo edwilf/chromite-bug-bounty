5577aa461f0eee211d9323b56eec0a7aa3b3a93a
edwilf <60770678+edwilf@users.noreply.github.com>
2021-12-27 21:39:31 +0530

    Add files via upload

---
+++ uatg_bypass_div_alu_logic.py
@@ -0,0 +1,62 @@
+from yapsy.IPlugin import IPlugin
+from uatg.instruction_constants import base_reg_file, arithmetic_instructions,mext_instructions\
+from typing import Dict, List, Union, Any
+import random
+
+
+class uatg_bypass_div_alu_logic(IPlugin):
+    """
+    This class contains methods to generate and validate the tests for bypassing from div to a
+    alu and vice versa with variable depths
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.isa = 'RV32I'
+        self.isa_bit = 'rv32'
+        self.offset_inc = 4
+        self.xlen = 32
+        self.num_rand_var = 100
+
+    def execute(self, core_yaml, isa_yaml) -> bool:
+        self.isa = isa_yaml['hart0']['ISA']
+        if 'RV32' in self.isa:
+            self.isa_bit = 'rv32'
+            self.xlen = 32
+            self.offset_inc = 4
+        else:
+            self.isa_bit = 'rv64'
+            self.xlen = 64
+            self.offset_inc = 8
+        return True
+
+    def generate_asm(self) -> Dict[str, str]:
+        reg_file = base_reg_file.copy()
+        test_dict = []
+
+        loop_count = 3
+        asm = "\n\tdiv x24,x20,x21 \n\n"
+        asm += f"\n\taddi t0, x0, {loop_count}\n\taddi t1,x0 ,0 \n\nloop:\n"
+        asm += "\taddi t1, t1, 1\n\tblt t1, t0, loop\n"
+        asm += "\n\t sub x25,x24,x1"
+        asm += "\n\t or x2,x3,x4"
+        asm += "\n\t div x24,x25,x21"
+        
+        compile_macros = []
+
+        test_dict.append({
+            'asm_code': asm,
+            'asm_sig' : '',
+            'compile_macros': compile_macros,
+            })
+
+
+    return test_dict
+
+        
+    def check_log(self, log_file_path, reports_dir) -> bool:
+        return False
+
+    def generate_covergroups(self, config_file) -> str:
+        sv = ""
+        return sv
